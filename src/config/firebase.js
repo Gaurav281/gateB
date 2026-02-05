3ee41507c6a9e3270d5a04e1c5807b8a5f9b5512
Gaurav281 <gauravpushpa28@gmail.com>
2026-02-05 13:20:21 +0530

    new changes

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -26,25 +26,3 @@ export const generateSignedUrl = async (filePath) => {
   return url;
 };
 
-
-
-
-
-
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBJIIe1y7Cy8TE7lDdkYj-0Kpv1FrNzWCs",
-//   authDomain: "gatepreppro.firebaseapp.com",
-//   projectId: "gatepreppro",
-//   storageBucket: "gatepreppro.firebasestorage.app",
-//   messagingSenderId: "44744910525",
-//   appId: "1:44744910525:web:6d8f26f1d23be9a6f3b9d4"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
