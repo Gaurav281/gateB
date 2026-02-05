b8fb029d5ad57b452e429e85fdd9ce04118de23b
Gaurav281 <gauravpushpa28@gmail.com>
2026-02-05 17:53:50 +0530

    new changes

--- src/app.js
+++ src/app.js
@@ -11,12 +11,33 @@ import adminRoutes from "./routes/admin.routes.js";
 const app = express();
 
 /* ---------------- Global Middleware ---------------- */
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://gatepreppro.vercel.app",
+  "https://gatepreppro.in",
+  "https://www.gatepreppro.in",
+];
+
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://gatepreppro.vercel.app", "http://gatepreppro.vercel.app",  "https://gatepreppro.in"], // frontend URL
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true); // allow Postman / server calls
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+// IMPORTANT: handle preflight
+app.options("*", cors());
+
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
